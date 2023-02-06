2264799fe5993496bf446b2f13e8809366fd07e1
bobik <104901009+alecsbobik@users.noreply.github.com>
2023-02-06 20:23:47 +0800

    Add files via upload

---
+++ JS/login.js
@@ -0,0 +1,46 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form_message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form_message-success", "form_message-error");
+    messageElement.classList.add(`form_message-${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // perform your AJAX/fetch Login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form_input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id == "signupUsername" && e.targer.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+    });
+
+});
